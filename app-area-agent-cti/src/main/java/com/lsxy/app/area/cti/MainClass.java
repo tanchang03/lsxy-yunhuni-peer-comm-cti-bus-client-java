27f6abe12e4584f1de6f3212bc21d34e6259093d
刘雪彦 <liuxy@yunhuni.com>
2016-08-05 20:37:56 +0800

    debug only

--- app-area-agent-cti/src/main/java/com/lsxy/app/area/cti/MainClass.java
+++ app-area-agent-cti/src/main/java/com/lsxy/app/area/cti/MainClass.java
@@ -22,52 +22,54 @@ public class MainClass {
         Client client = Unit.createClient((byte) 0, "192.168.2.100",
                 request -> logger.debug("收到事件：{}", request)
         );
-        for (int i = 0; i < 1; ++i) {
-            Map<String, Object> params = new HashMap<>();
-            params.put("from_uri", "");
-            params.put("to_uri", "192.168.2.100:5062");
-            params.put("max_answer_seconds", (int) (50 * Math.random()));
-            params.put("max_ring_seconds", (int) (10 * Math.random()));
 
-            client.createResource(0, 0, "sys.call", params, new RpcResultListener() {
-                @Override
-                protected void onResult(Object result) {
-                    logger.debug("呼出 返回值：(result={})", result);
-                    String callId = (String) result;
-                    Map<String, Object> params = new HashMap<>();
-                    try {
-                        client.operateResource((byte) 0, 0, callId, "sys.call.drop", params, new RpcResultListener() {
-                            @Override
-                            protected void onResult(Object result) {
-                                logger.debug("挂机 返回值：(result={})", result);
-                            }
 
-                            @Override
-                            protected void onError(RpcError error) {
-                                logger.debug("挂机 错误:{}", error);
-                            }
-
-                            @Override
-                            protected void onTimeout() {
-                                logger.debug("挂机 超时");
-                            }
-                        });
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                protected void onError(RpcError error) {
-                    logger.error("呼出 错误:{}", error);
-                }
-
-                @Override
-                protected void onTimeout() {
-                    logger.error("呼出 超时");
-                }
-            });
-        }
+//        for (int i = 0; i < 1; ++i) {
+//            Map<String, Object> params = new HashMap<>();
+//            params.put("from_uri", "");
+//            params.put("to_uri", "192.168.2.100:5062");
+//            params.put("max_answer_seconds", (int) (50 * Math.random()));
+//            params.put("max_ring_seconds", (int) (10 * Math.random()));
+//
+//            client.createResource(0, 0, "sys.call", params, new RpcResultListener() {
+//                @Override
+//                protected void onResult(Object result) {
+//                    logger.debug("呼出 返回值：(result={})", result);
+//                    String callId = (String) result;
+//                    Map<String, Object> params = new HashMap<>();
+//                    try {
+//                        client.operateResource((byte) 0, 0, callId, "sys.call.drop", params, new RpcResultListener() {
+//                            @Override
+//                            protected void onResult(Object result) {
+//                                logger.debug("挂机 返回值：(result={})", result);
+//                            }
+//
+//                            @Override
+//                            protected void onError(RpcError error) {
+//                                logger.debug("挂机 错误:{}", error);
+//                            }
+//
+//                            @Override
+//                            protected void onTimeout() {
+//                                logger.debug("挂机 超时");
+//                            }
+//                        });
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//
+//                @Override
+//                protected void onError(RpcError error) {
+//                    logger.error("呼出 错误:{}", error);
+//                }
+//
+//                @Override
+//                protected void onTimeout() {
+//                    logger.error("呼出 超时");
+//                }
+//            });
+//        }
 
         String inputStr;
         Scanner scanner = new Scanner(System.in);
