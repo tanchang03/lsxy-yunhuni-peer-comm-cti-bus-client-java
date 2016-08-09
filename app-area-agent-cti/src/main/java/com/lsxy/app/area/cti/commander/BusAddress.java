8cac13b89d7fad5e4683fd65d5ddda1deda8a5ef
刘雪彦 <liuxy@yunhuni.com>
2016-08-09 17:29:09 +0800

    add some comments
    (cherry picked from commit 33678e706b746b4ff9c2d369a73cdbd0c12c0565)

--- app-area-agent-cti/src/main/java/com/lsxy/app/area/cti/commander/BusAddress.java
+++ app-area-agent-cti/src/main/java/com/lsxy/app/area/cti/commander/BusAddress.java
@@ -16,10 +16,16 @@ public class BusAddress {
     private final byte unitId;
     private final byte clientId;
 
+    /**
+     * @return CTI BUS 地址的 Unit 部分
+     */
     public byte getUnitId() {
         return unitId;
     }
 
+    /**
+     * @return CTI BUS 地址的 Client 部分
+     */
     public byte getClientId() {
         return clientId;
     }
