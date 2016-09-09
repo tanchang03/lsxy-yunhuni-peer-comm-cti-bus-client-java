954b8e78ffb599710b650011653e0fa37586f785
刘雪彦 <liuxy@yunhuni.com>
2016-09-09 11:44:33 +0800

    增加: ipsc事件的详细文本日志

--- app-area-agent-cti/src/main/java/com/lsxy/app/area/cti/LibCallbackHandler.java
+++ app-area-agent-cti/src/main/java/com/lsxy/app/area/cti/LibCallbackHandler.java
@@ -57,7 +57,7 @@ class LibCallbackHandler implements com.lsxy.app.area.cti.busnetcli.Callbacks {
     }
 
     public void data(Head head, byte[] bytes) {
-        logger.debug(">>> data(head={}, dataLength={}: {})", head, bytes.length, bytes.toString());
+        logger.debug(">>> data(head={}, dataLength={})", head, bytes.length);
         byte dstType = head.getDstClientType();
         if (dstType == (byte) 3) {
             Monitor monitor = (Monitor) Unit.clients.get(head.getDstClientId());
@@ -68,6 +68,7 @@ class LibCallbackHandler implements com.lsxy.app.area.cti.busnetcli.Callbacks {
             String data = null;
             try {
                 data = new String(bytes, "ASCII");
+                logger.debug("data={}", data);
             } catch (UnsupportedEncodingException ignore) {
             }
             String finalData = data;
